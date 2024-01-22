62db02eec26d190c86ddda474fd8e54270f9f17e
lsameerdheen <ameerdeen@gmail.com>
2024-01-22 12:33:18 +0530

    Console client App baseline

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿namespace consoleclient;
+using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+           
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World! - client");
+        TcpClient client = new TcpClient("127.0.0.1", 5555);
+        NetworkStream stream = client.GetStream();
+           using (StreamReader reader = new StreamReader(stream))
+            using (StreamWriter writer = new StreamWriter(stream))
+            {
+                String response = null ;
+                do{
+                Console.WriteLine("Write something to send to server:");
+                String input = Console.ReadLine();
+                writer.WriteLine(input);
+                writer.Flush();
+              
+                response = reader.ReadLine();
+                Console.WriteLine("Server said: {0}", response);  
+                Boolean status = response !=null &&  response  != "see you!!";              
+                Console.WriteLine("Connection status", status);  
+                }while(response !=null &&  response  != "see you!!");
+            }
+          
+            Console.Write("Press any key to continue . . . ");
+            Console.ReadKey(true);
+
+    }
+}
